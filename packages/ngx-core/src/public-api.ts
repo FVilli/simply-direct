9f531af378310f188657bbd956694d23d53b8666
Federico Villi <fvilli@gmail.com>
2025-04-23 15:00:23 +0000

    .

--- packages/ngx-core/src/public-api.ts
+++ packages/ngx-core/src/public-api.ts
@@ -1,10 +1,6 @@
 /*
- * Public API Surface of ngx-core-service
+ * Public API Surface of ngx-core
  */
 
-export * from './lib/core.service';
-export * from './lib/functions';
-export * from './lib/store';
-export * from './lib/auth';
-export * from './lib/with-crud-entities';
-export * from './lib/with-lookup-entities';
+export * from './lib/ngx-core.service';
+export * from './lib/ngx-core.component';
---
+++ packages/ngx-core_old/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of ngx-core-service
+ */
+
+export * from './lib/core.service';
+export * from './lib/functions';
+export * from './lib/store';
+export * from './lib/auth';
+export * from './lib/with-crud-entities';
+export * from './lib/with-lookup-entities';
